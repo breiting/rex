66f759f911f81fa603e3a1e440e5f1b67859becd
Bernhard Reitinger <br@reecon.at>
2018-08-09 13:07:43 +0200

    Tweak users api

--- user.go
+++ user.go
@@ -6,46 +6,61 @@ import (
 	"io"
 	"io/ioutil"
 	"net/http"
+
+	"github.com/tidwall/gjson"
 )
 
-// User is the basic structure for the current user information
+// User is the basic structure for a REX user
+//
+// The user can either contain the currentUser information,
+// but also information from a user query. The self link can be
+// used to directly access the data, but is also often requred
+// for other operations (e.g. insert a project)
 type User struct {
 	UserID    string   `json:"userId"`
-	Username  string   `json:"username"`
-	Email     string   `json:"email"`
-	FirstName string   `json:"firstName"`
-	LastName  string   `json:"lastName"`
-	Roles     []string `json:"roles"`
+	Username  string   `json:"username,omitempty"`
+	Email     string   `json:"email,omitempty"`
+	FirstName string   `json:"firstName,omitempty"`
+	LastName  string   `json:"lastName,omitempty"`
+	LastLogin string   `json:"lastLogin,omitempty"`
+	Roles     []string `json:"roles,omitempty"`
 	Links     struct {
 		User struct {
 			Href string `json:"href"`
 		} `json:"user"`
-	} `json:"_links"`
+	} `json:"_links,omitempty"`
 }
 
 var (
 	apiCurrentUser = "/api/v2/users/current"
+	apiUsers       = "/api/v2/users"
+	apiFindByEmail = "/api/v2/users/search/findUserIdByEmail?email="
+	apiFindByID    = "/api/v2/users/search/findByUserId?userId="
 )
 
+// String nicely prints out the user information
 func (u User) String() string {
-	s := fmt.Sprintf("| UserId   | %-65s |\n", u.UserID)
-	s += fmt.Sprintf("| Username | %-65s |\n", u.Username)
-	s += fmt.Sprintf("| Firstname| %-65s |\n", u.FirstName)
-	s += fmt.Sprintf("| Lastname | %-65s |\n", u.LastName)
-	s += fmt.Sprintf("| Email    | %-65s |\n", u.Email)
-	s += fmt.Sprintf("| Link     | %-65s |\n", u.Links.User.Href)
+	s := fmt.Sprintf("| UserId    | %-65s |\n", u.UserID)
+	s += fmt.Sprintf("| Username  | %-65s |\n", u.Username)
+	s += fmt.Sprintf("| Firstname | %-65s |\n", u.FirstName)
+	s += fmt.Sprintf("| Lastname  | %-65s |\n", u.LastName)
+	s += fmt.Sprintf("| Email     | %-65s |\n", u.Email)
+	s += fmt.Sprintf("| LastLogin | %-65s |\n", u.LastLogin)
+	s += fmt.Sprintf("| Link      | %-65s |\n", u.Links.User.Href)
 
 	return s
 }
 
 // GetCurrentUser gets the user details of the current user
-func GetCurrentUser(c *Client) User {
+//
+// The current user is the one which has been identified by the authentication token.
+func GetCurrentUser(c *Client) (*User, error) {
 	req, _ := http.NewRequest("GET", RexBaseURL+apiCurrentUser, nil)
 	c.token.SetAuthHeader(req)
 
 	resp, err := c.httpClient.Do(req)
 	if err != nil {
-		panic(err)
+		return nil, err
 	}
 	defer func() {
 		io.Copy(ioutil.Discard, resp.Body)
@@ -53,14 +68,68 @@ func GetCurrentUser(c *Client) User {
 
 	body, err := ioutil.ReadAll(resp.Body)
 	if err != nil {
-		panic(err)
+		return nil, err
 	}
 
 	var u User
 	err = json.Unmarshal(body, &u)
+	return &u, err
+}
 
+// GetTotalNumberOfUsers returns the number of registered users
+//
+// Requires admin permissions
+func GetTotalNumberOfUsers(c *Client) (uint64, error) {
+	req, _ := http.NewRequest("GET", RexBaseURL+apiUsers, nil)
+	c.token.SetAuthHeader(req)
+
+	resp, err := c.httpClient.Do(req)
 	if err != nil {
-		panic(err)
+		return 0, err
 	}
-	return u
+	defer func() {
+		io.Copy(ioutil.Discard, resp.Body)
+	}()
+
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return 0, err
+	}
+
+	return gjson.Get(string(body), "page.totalElements").Uint(), nil
+}
+
+// GetUserByEmail retrieves the user information based on the email address
+func GetUserByEmail(c *Client, email string) (*User, error) {
+
+	req, _ := http.NewRequest("GET", RexBaseURL+apiFindByEmail+email, nil)
+	c.token.SetAuthHeader(req)
+
+	r, err := c.httpClient.Do(req)
+	if err != nil {
+		return nil, err
+	}
+
+	// check if the user can be found
+	var user User
+	err = json.NewDecoder(r.Body).Decode(&user)
+	io.Copy(ioutil.Discard, r.Body)
+
+	if err != nil || user.UserID == "" {
+		return &User{}, fmt.Errorf("user not found")
+	}
+
+	// Fetch actual user information based on the retrieved UserID
+	req, _ = http.NewRequest("GET", RexBaseURL+apiFindByID+user.UserID, nil)
+	c.token.SetAuthHeader(req)
+	r, err = c.httpClient.Do(req)
+	if err != nil {
+		return nil, err
+	}
+	defer func() {
+		io.Copy(ioutil.Discard, r.Body)
+	}()
+
+	err = json.NewDecoder(r.Body).Decode(&user)
+	return &user, nil
 }
