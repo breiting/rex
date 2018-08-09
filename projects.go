6f016df6f618324b2eb0fb263ab5839e78a6ea17
Bernhard Reitinger <br@reecon.at>
2018-08-09 17:26:41 +0200

    Finish up uploading project file with content

--- projects.go
+++ projects.go
@@ -8,8 +8,9 @@ import (
 	"io/ioutil"
 	"mime/multipart"
 	"net/http"
-	"os"
-	"path/filepath"
+	"regexp"
+
+	"github.com/tidwall/gjson"
 )
 
 // Reference is currently a simple struct which only contains the key
@@ -22,6 +23,7 @@ type Reference struct {
 
 // ProjectSimple is the basic structure representing a simple RexProject
 type ProjectSimple struct {
+	ID    string // only set by the client for convenience
 	Name  string `json:"name"`
 	Owner string `json:"owner"`
 	Links struct {
@@ -41,13 +43,15 @@ type ProjectSimpleList struct {
 var (
 	apiProjects       = "/api/v2/projects"
 	apiProjectByOwner = "/api/v2/projects/search/findAllByOwner?owner="
+	apiProjectFiles   = "/api/v2/projectFiles/"
 )
 
 // String nicely prints a list of projects
 func (p ProjectSimpleList) String() string {
 	var s string
+	s += fmt.Sprintf("| %6s | %-20s | %-15s | %65s |\n", "ID", "Name", "Owner", "Self Link")
 	for _, proj := range p.Embedded.Projects {
-		s += fmt.Sprintf("| %-20s | %-15s | %65s |\n", proj.Name, proj.Owner, proj.Links.Self.Href)
+		s += fmt.Sprintf("| %6s | %-20s | %-15s | %65s |\n", proj.ID, proj.Name, proj.Owner, proj.Links.Self.Href)
 	}
 	return s
 }
@@ -67,6 +71,15 @@ func GetProjects(c *Client) (*ProjectSimpleList, error) {
 
 	var projects ProjectSimpleList
 	err = json.NewDecoder(resp.Body).Decode(&projects)
+
+	// set ID for convenience
+	for i, p := range projects.Embedded.Projects {
+		re, _ := regexp.Compile("/projects/(.*)")
+		values := re.FindStringSubmatch(p.Links.Self.Href)
+		if len(values) > 0 {
+			projects.Embedded.Projects[i].ID = values[1]
+		}
+	}
 	return &projects, err
 }
 
@@ -96,8 +109,62 @@ func CreateProject(c *Client, name string) error {
 	return nil
 }
 
-// TODO test code
-func UpdateProjectFile(c *Client) {
+// UploadProjectFiles uploads a list of local files to a given project
+func UploadProjectFiles(c *Client, projectID string, name string, fileName string, r io.Reader) error {
+
+	projectFile := struct {
+		Name    string `json:"name"`
+		Project string `json:"project"`
+	}{
+		Name:    name,
+		Project: RexBaseURL + apiProjects + "/" + projectID,
+	}
+
+	// Create project file
+	b := new(bytes.Buffer)
+	json.NewEncoder(b).Encode(projectFile)
+
+	req, _ := http.NewRequest("POST", RexBaseURL+apiProjectFiles, b)
+	req.Header.Add("accept", "application/json")
+	c.token.SetAuthHeader(req)
+
+	resp, err := c.httpClient.Do(req)
+	if err != nil {
+		return err
+	}
+
+	body, _ := ioutil.ReadAll(resp.Body)
+	// fmt.Println(string(body))
+
+	if resp.StatusCode != 201 {
+		return fmt.Errorf("Got server status %d with error: %s ", resp.StatusCode, body)
+	}
+
+	// Upload the actual payload
+	uploadURL := gjson.Get(string(body), "_links.file\\.upload.href").String()
+	io.Copy(ioutil.Discard, resp.Body)
+	return uploadFileContent(c, uploadURL, fileName, r)
+}
+
+func uploadFileContent(c *Client, uploadURL string, fileName string, r io.Reader) error {
+
+	body := &bytes.Buffer{}
+	writer := multipart.NewWriter(body)
+	part, _ := writer.CreateFormFile("file", fileName)
+	io.Copy(part, r)
+	writer.Close()
+
+	req, _ := http.NewRequest("POST", uploadURL, body)
+	c.token.SetAuthHeader(req)
+	req.Header.Add("Content-Type", writer.FormDataContentType())
+
+	resp, err := c.httpClient.Do(req)
+	io.Copy(ioutil.Discard, resp.Body)
+	return err
+}
+
+// UpdateProjectFile - test code
+func updateProjectFile(c *Client) {
 	var body = []byte(`{"type": "rex"}`)
 	req, _ := http.NewRequest("PATCH", RexBaseURL+"/api/v2/projectFiles/1044", bytes.NewBuffer(body))
 	c.token.SetAuthHeader(req)
@@ -107,29 +174,4 @@ func UpdateProjectFile(c *Client) {
 		panic(err)
 	}
 	fmt.Println(resp.Status)
-}
-
-// TODO test code
-func UploadFileContent(c *Client, filePath string) {
-
-	file, _ := os.Open(filePath)
-	defer file.Close()
-
-	body := &bytes.Buffer{}
-	writer := multipart.NewWriter(body)
-	part, _ := writer.CreateFormFile("file", filepath.Base(file.Name()))
-	io.Copy(part, file)
-	writer.Close()
-
-	req, _ := http.NewRequest("POST", RexBaseURL+"/api/v2/projectFiles/1044/file", body)
-	c.token.SetAuthHeader(req)
-	req.Header.Add("Content-Type", writer.FormDataContentType())
-
-	resp, err := c.httpClient.Do(req)
-	if err != nil {
-		panic(err)
-	}
-	defer func() {
-		io.Copy(ioutil.Discard, resp.Body)
-	}()
 }
