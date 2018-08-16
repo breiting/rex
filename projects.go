6755e46509be9ec6986b17531a8dba532f5f85c7
Bernhard Reitinger <br@reecon.at>
2018-08-16 13:04:49 +0200

    Create RexReference for every ProjectFile which gets created
    By creating a new ProjectFile, the library now also creates a valid
    RexReference with a fileTransformation.
    
    Currently, absolute and relative transformation is not supported for
    ProjectFile.

--- projects.go
+++ projects.go
@@ -17,10 +17,15 @@ import (
 )
 
 // Reference is a spatial anchor which can be attached to a project or a project file.
-//
-// Currently this feature is not yet implemented fully.
 type Reference struct {
-	Key string `json:"key"`
+	Key             string                 `json:"key"`
+	Project         string                 `json:"project"`
+	ParentReference string                 `json:"parentReference"`
+	RootReference   bool                   `json:"rootReference"`
+	Address         *ProjectAddress        `json:"address"`
+	AbsTransform    *ProjectTransformation `json:"absoluteTransformation"`
+	RelTransform    *ProjectTransformation `json:"relativeTransformation"`
+	FileTransform   *FileTransformation    `json:"fileTransformation"`
 }
 
 // ProjectSimple is the basic structure representing a simple RexProject
@@ -70,6 +75,20 @@ type ProjectTransformation struct {
 	} `json:"position"`
 }
 
+// FileTransformation is used for defining the relationship between the RexReference and the actual file.
+type FileTransformation struct {
+	Rotation struct {
+		X float64 `json:"x"`
+		Y float64 `json:"y"`
+		Z float64 `json:"z"`
+	} `json:"rotation"`
+	Position struct {
+		Type        string    `json:"type"`
+		Coordinates []float64 `json:"coordinates"`
+	} `json:"position"`
+	Scale float64 `json:"scale"`
+}
+
 var (
 	apiProjects       = "/api/v2/projects"
 	apiRexReferences  = "/api/v2/rexReferences"
@@ -116,6 +135,27 @@ func GetProjects(e Executor, userID string) (*ProjectSimpleList, error) {
 	return &projects, err
 }
 
+// Creates a new RexReference using the REX API
+func createRexReference(e Executor, r *Reference) (string, error) {
+
+	b := new(bytes.Buffer)
+	json.NewEncoder(b).Encode(r)
+
+	req, _ := http.NewRequest("POST", RexBaseURL+apiRexReferences, b)
+	resp, err := e.Execute(req)
+	defer func() {
+		io.Copy(ioutil.Discard, resp.Body)
+	}()
+	if err != nil {
+		return "", err
+	}
+	body, _ := ioutil.ReadAll(resp.Body)
+	if resp.StatusCode != 201 {
+		return "", fmt.Errorf("Got server status %d with error: %s ", resp.StatusCode, body)
+	}
+	return gjson.Get(string(body), "_links.self.href").String(), nil
+}
+
 // CreateProject creates a new project for the current user.
 //
 // The name is used as project name
@@ -126,8 +166,6 @@ func CreateProject(e Executor, userID, name string, address *ProjectAddress, abs
 	json.NewEncoder(b).Encode(p)
 
 	req, _ := http.NewRequest("POST", RexBaseURL+apiProjects, b)
-	req.Header.Add("accept", "application/json")
-
 	resp, err := e.Execute(req)
 	if err != nil {
 		return err
@@ -143,38 +181,16 @@ func CreateProject(e Executor, userID, name string, address *ProjectAddress, abs
 	uuid := uuid.New().String()
 
 	// Create a RexReference as well
-	rexReference := struct {
-		Project                string                 `json:"project"`
-		RootReference          bool                   `json:"rootReference"`
-		Key                    string                 `json:"key"`
-		Address                *ProjectAddress        `json:"address"`
-		AbsoluteTransformation *ProjectTransformation `json:"absoluteTransformation"`
-	}{
-		Project:                projectSelfLink,
-		RootReference:          true,
-		Key:                    uuid,
-		Address:                address,
-		AbsoluteTransformation: absoluteTransformation,
+	rexReference := Reference{
+		Project:       projectSelfLink,
+		RootReference: true,
+		Key:           uuid,
+		Address:       address,
+		AbsTransform:  absoluteTransformation,
 	}
 
-	json.NewEncoder(b).Encode(rexReference)
-
-	req, _ = http.NewRequest("POST", RexBaseURL+apiRexReferences, b)
-	req.Header.Add("accept", "application/json")
-
-	resp, err = e.Execute(req)
-	defer func() {
-		io.Copy(ioutil.Discard, resp.Body)
-	}()
-	if err != nil {
-		return err
-	}
-
-	if resp.StatusCode != 201 {
-		return fmt.Errorf("Got server status %d with error: %s ", resp.StatusCode, body)
-	}
-
-	return nil
+	_, err = createRexReference(e, &rexReference)
+	return err
 }
 
 // UploadProjectFile uploads a new project file.
@@ -182,31 +198,67 @@ func CreateProject(e Executor, userID, name string, address *ProjectAddress, abs
 // The project is identified by the projectID (e.g. 1020). The file requires a name,
 // which is displayed, but also a fileName which includes the suffix. The fileName is used
 // for detecting the mimetype. The content of the file will be read from the io.Reader r.
-func UploadProjectFile(e Executor, projectID string, name string, fileName string, r io.Reader) error {
+func UploadProjectFile(e Executor, projectID string, name string, fileName string, transform *FileTransformation, r io.Reader) error {
 
-	projectFile := struct {
-		Name    string `json:"name"`
-		Project string `json:"project"`
-	}{
-		Name:    name,
-		Project: RexBaseURL + apiProjects + "/" + projectID,
-	}
-
-	// Create project file
 	b := new(bytes.Buffer)
-	json.NewEncoder(b).Encode(projectFile)
 
-	req, _ := http.NewRequest("POST", RexBaseURL+apiProjectFiles, b)
+	// IMPORTANT:
+	// Since RexReference and ProjectFile is a 1..n relationship, we have
+	// to create the RexReference before we create the ProjectFile
 
+	// Query the project reference (required)
+	rootReferenceURL := RexBaseURL + apiProjects + "/" + projectID + "/rootRexReference"
+	req, _ := http.NewRequest("GET", rootReferenceURL, b)
 	resp, err := e.Execute(req)
 	if err != nil {
 		return err
 	}
-
+	// Check if root reference is available, spit error if not!
+	if resp.StatusCode != 200 {
+		io.Copy(ioutil.Discard, resp.Body)
+		return fmt.Errorf("Cannot create project file reference, because no project reference is set")
+	}
 	body, _ := ioutil.ReadAll(resp.Body)
-	// fmt.Println(string(body))
+	io.Copy(ioutil.Discard, resp.Body)
+
+	parentReferenceURL := gjson.Get(string(body), "_links.self.href").String()
+
+	// Create a RexReference as well
+	uuid := uuid.New().String()
+	rexReference := Reference{
+		Project:         RexBaseURL + apiProjects + "/" + projectID,
+		RootReference:   false,
+		ParentReference: parentReferenceURL,
+		Key:             uuid,
+		FileTransform:   transform,
+	}
+
+	selfLink, err := createRexReference(e, &rexReference)
+	if err != nil {
+		return err
+	}
+
+	projectFile := struct {
+		Name         string `json:"name"`
+		Project      string `json:"project"`
+		RexReference string `json:"rexReference"`
+	}{
+		Name:         name,
+		Project:      RexBaseURL + apiProjects + "/" + projectID,
+		RexReference: selfLink,
+	}
+
+	// Create project file
+	json.NewEncoder(b).Encode(projectFile)
+	req, _ = http.NewRequest("POST", RexBaseURL+apiProjectFiles, b)
+	resp, err = e.Execute(req)
+	if err != nil {
+		return err
+	}
+	body, _ = ioutil.ReadAll(resp.Body)
 
 	if resp.StatusCode != 201 {
+		io.Copy(ioutil.Discard, resp.Body)
 		return fmt.Errorf("Got server status %d with error: %s ", resp.StatusCode, body)
 	}
 
